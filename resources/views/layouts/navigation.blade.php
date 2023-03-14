6e39a8e2238fdb23cccf4775c01ea0f8639cf57b
GoldStar731 <seniordev213@gmail.com>
2023-03-14 11:52:42 +0900

    UI Change

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,25 +1,38 @@
-<div class="myHeader h-24 flex items-center">
-    <div class="container max-w-7xl flex justify-between items-center">
-        <div class="myLogo">
-            <a href="/dashboard">
-                <img src="{{ asset('./assets/img/common/logo.png') }}" alt="" class="w-16">
-            </a>
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center shadow">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <a href="/" class="logo d-flex align-items-center me-auto me-lg-0">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1>SGDs</h1>
+        </a>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a href="/">ホーム</a></li>
+                <li><a href="{{ route('markets.index') }}">市場</a></li>
+                <li><a href="{{ route('transportCompanies.index') }}">運送会社</a></li>
+                <li><a href="{{ route('pastorals.index') }}">牧場</a></li>
+                <li><a href="{{ route('slaughterHouses.index') }}">屠殺場</a></li>
+                <li><a href="{{ route('parts.index') }}">パーツカット</a></li>
+                <li><a href="{{ route('purchases.index') }}">ユーザーページ</a></li>
+            </ul>
+        </nav><!-- .navbar -->
+
+        @if (Route::has('login'))
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            @auth
+            <a class="btn-book-a-table" style="margin-left: 0;" href="/logout">ログアウト</a>
+            @else
+            <a class="btn-book-a-table" style="margin-left: 0;" href="{{ route('login') }}">ログイン</a>
+            <a class="btn-book-a-table" style="margin-left: 0;" href="{{ route('register') }}">会員登録</a>
+            @endauth
         </div>
-        <div class="myHeaderMenu">
-            <div class="menuItem">
-                <a class="p-3" href="/">ホーム</a>
-                <a class="p-3" href="{{ route('markets.index') }}">市場</a>
-                <a class="p-3" href="{{ route('transportCompanies.index') }}">運送会社</a>
-                <a class="p-3" href="{{ route('pastorals.index') }}">パストラル</a>
-                <a class="p-3" href="{{ route('slaughterHouses.index') }}">スローターハウス</a>
-                <a class="p-3" href="{{ route('parts.index') }}">パーツカット</a>
-                @auth
-                <a class="p-3 pr-0" href="/logout">ログアウト</a>
-                @else
-                <a class="p-3" href="/login">ログイン</a>
-                <a class="p-3 pr-0" href="/register">会員登録</a>
-                @endauth
-            </div>
-        </div>
+        @endif
+
+        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
     </div>
-</div>
+</header><!-- End Header -->
