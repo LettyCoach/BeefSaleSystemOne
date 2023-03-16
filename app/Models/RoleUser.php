44173a7a4fab21ceb2f60a290e61ab3bff329911
dev star <moriyamayukio85@gmail.com>
2023-03-17 00:59:40 +0900

    small change

--- app/Models/RoleUser.php
+++ app/Models/RoleUser.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserRole extends Model
+class RoleUser extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'role_id',
+    ];
 }
